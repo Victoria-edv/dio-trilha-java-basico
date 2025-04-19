e2e57d8dc7fa3f21b3999cbfd4fafae3e868da72
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-04-19 18:18:07 -0300

    #

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        //criando o objeto scanner
+    
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
         System.out.println("Por favor, digite seu nome! ");
@@ -18,8 +18,6 @@ public class ContaTerminal {
         System.out.println("Agora, digite o valor que deseja retirar: ");
         double saldo = scanner.nextDouble();
 
-        
-        //imprimindo os dados obtidos pelo usuario
         System.out.println("Ola " + nome + ", obrigada por criar uma conta em nosso banco, sua agência " + agencia + " sua conta " + conta + " e seu saldo " + saldo + " já está disponível para saque!");
         
         
